7c60c285f6197202380d5e082af8bfe36eba7056
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-12-14 16:44:03 +0100

    Create the blank project from the VS template
    - UWP App (blank)
    - Target: 17XX (min 15063)

---
+++ IoTLab-Temperatures/IoTLab-Temperatures/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Déclaration de la classe MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace IoTLab_Temperatures
+{
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
