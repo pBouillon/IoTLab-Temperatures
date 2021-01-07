86aeb28a2f4a3dead468e09a2174ce3fba81a99d
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-01-07 19:22:07 +0100

    Create the GeographicCoordinate class

---
+++ IoTLab-Temperatures/IoTLab-Temperatures/GeographicCoordinate.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "GeographicCoordinate.h"
+
+// Create a new instance of a geographic coordinate, based on the provided
+// angles. If any of the provided angles is an illegal value, throws an invalid_argument.
+// For the validity of the coordinates, refer to:
+// https://en.wikipedia.org/wiki/Geographic_coordinate_system#Latitude_and_longitude
+GeographicCoordinate::GeographicCoordinate(double latitude, double longitude)
+{
+	if (!IsValidLatitude(latitude))
+		throw std::invalid_argument("Incorrect latitude provided");
+
+	this->latitude = latitude;
+
+	if (!IsValidLongitude(longitude))
+		throw std::invalid_argument("Incorrect longitude provided");
+
+	this->longitude = longitude;
+}
+
+double GeographicCoordinate::GetLatitude()
+{
+	return this->latitude;
+}
+
+double GeographicCoordinate::GetLongitude()
+{
+	return this->longitude;
+}
+
+bool GeographicCoordinate::IsValidLatitude(double latitude)
+{
+	return -MAX_ABSOLUTE_LATITUDE_DEGREE <= latitude
+		&& latitude <= MAX_ABSOLUTE_LATITUDE_DEGREE;
+}
+
+bool GeographicCoordinate::IsValidLongitude(double longitude)
+{
+	return -MAX_ABSOLUTE_LONGITUDE_DEGREE <= longitude
+		&& longitude <= MAX_ABSOLUTE_LONGITUDE_DEGREE;
+}
---
+++ IoTLab-Temperatures/IoTLab-Temperatures/GeographicCoordinate.h
@@ -0,0 +1,36 @@
+#pragma once
+
+// Maximum absolute degree of the latitude
+const int MAX_ABSOLUTE_LATITUDE_DEGREE = 90;
+
+// Maximum absolute degree of the longitude
+const int MAX_ABSOLUTE_LONGITUDE_DEGREE = 180;
+
+// Create a new instance of a geographic coordinate.
+// Example:
+//     GeographicCoordinate coordinate (49.59933, 6.10567)
+class GeographicCoordinate
+{
+	// Latitude of the geographic coordinate
+	double latitude;
+
+	// Longitude of the geographic coordinate
+	double longitude;
+
+public:
+	// Create a new instance of a geographic coordinate, based on the provided
+	// angles.
+	GeographicCoordinate(double, double);
+
+	// Get the latitude
+	double GetLatitude();
+
+	// Get the longitude
+	double GetLongitude();
+
+	// Check if a latitude is within the allowed bounds of +- MAX_ABSOLUTE_LATITUDE_DEGREE
+	static bool IsValidLatitude(double);
+
+	// Check if a longitude is within the allowed bounds of +- MAX_ABSOLUTE_LONGITUDE_DEGREE
+	static bool IsValidLongitude(double);
+};
