4aa21a4d24829ea27b79bc9b3c330495f1966816
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-01-09 17:49:11 +0100

    WiP - Naive implementation of the HTTP call

--- IoTLab-Temperatures/IoTLab-Temperatures/MainPage.xaml.cpp
+++ IoTLab-Temperatures/IoTLab-Temperatures/MainPage.xaml.cpp
@@ -3,13 +3,27 @@
 // Implementation of the MainPage class.
 //
 
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <codecvt>
+#include <iterator>
+#include <locale> 
+#include <shared_mutex>
+#include <stdio.h>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <vector>
+
 #include "pch.h"
 #include "MainPage.xaml.h"
 #include "GeographicCoordinate.h"
 
 using namespace IoTLab_Temperatures;
 
+using namespace Concurrency;
 using namespace Platform;
+using namespace Windows::Data::Json;
 using namespace Windows::Foundation;
 using namespace Windows::Foundation::Collections;
 using namespace Windows::UI::Xaml;
@@ -19,13 +33,15 @@ using namespace Windows::UI::Xaml::Data;
 using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
-
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Web::Http;
 
 
 // Separator used when displaying alongside the latitude and the longitude of the user
 const Platform::String^ GEOGRAPHIC_COORDINATE_SEPARATOR = ", ";
+
+// IoTLab API URI
+const Platform::String^ IOTLAB_URI = "http://iotlab.telecomnancy.eu:8080/iotlab/rest";
 
 // Maximum size allowed for the latitude and the longitude input (ex: "12.34567")
 const int INPUT_MAX_SIZE = 8;
@@ -72,6 +88,33 @@ void IoTLab_Temperatures::MainPage::LongitudeBox_TextChanged(
 	Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e)
 {
 	UpdateValidateButtonValidity();
+}
+
+
+void IoTLab_Temperatures::MainPage::RetrieveTemperatureFromIoTLab()
+{
+	Platform::String^ url = IOTLAB_URI + "/data/1/temperature-light2-light1-battery_indicator-humidity/1/";
+	Windows::Foundation::Uri^ uri = ref new Uri(url);
+
+	Windows::Web::Http::HttpClient^ httpClient = ref new HttpClient();
+
+	create_task(httpClient->GetStringAsync(uri))
+		.then([=](Platform::String^ Tet) 
+	{
+		auto objJson = JsonObject::Parse(Tet);
+		
+		JsonArray^ data = objJson->GetObject()->GetNamedArray("data")->GetArray();
+		
+		int jsonArraySize = data->Size;
+		
+		for (int i = 0; i < jsonArraySize; i++) {
+			if (data->GetObjectAt(i)->GetNamedString("label")->Equals("temperature")) {
+				Platform::String^ temprature = data->GetObjectAt(i)->GetNamedValue("value")->ToString();
+			}
+		}
+
+		return task_from_result();
+	});
 }
 
 
--- IoTLab-Temperatures/IoTLab-Temperatures/MainPage.xaml.h
+++ IoTLab-Temperatures/IoTLab-Temperatures/MainPage.xaml.h
@@ -20,6 +20,7 @@ namespace IoTLab_Temperatures
 		bool IsLongitudeValid();
 		void LatitudeBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
 		void LongitudeBox_TextChanged(Platform::Object^ sender, Windows::UI::Xaml::Controls::TextChangedEventArgs^ e);
+		void RetrieveTemperatureFromIoTLab();
 		double ToDouble(Platform::String ^ value);
 		void UpdateValidateButtonValidity();
 		void ValidateButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
