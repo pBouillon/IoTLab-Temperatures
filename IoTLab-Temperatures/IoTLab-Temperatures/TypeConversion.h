c2f1c28f13040df6ef459004a51dc2b7d307eceb
pBouillon <pro.pierre.bouillon@protonmail.com>
2021-01-20 19:22:30 +0100

    Add a method to convert a double to a string up to a certain precision

--- IoTLab-Temperatures/IoTLab-Temperatures/TypeConversion.h
+++ IoTLab-Temperatures/IoTLab-Temperatures/TypeConversion.h
@@ -5,6 +5,8 @@ namespace typeConversion
 	double ToDouble(Platform::String^ value);
 
 	Platform::String^ ToPlatformString(const std::string& input);
+	
+	Platform::String^ ToPlatformStringWithPrecision(double value, unsigned int precision = 2);
 
 	std::string ToString(Platform::String^ value);
 }
